49e13479303d65620296bd2d9efd27684aef36c8
Mark Chavez <markchav3z@gmail.com>
2017-08-09 11:17:58 +0800

    use agents

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -2,7 +2,7 @@ defmodule Dictionary do
 
   alias Dictionary.WordList
 
-  defdelegate start(),       to: WordList, as: :word_list
+  defdelegate start(),                to: WordList, as: :word_list
   defdelegate random_word(word_list), to: WordList
 
 end
